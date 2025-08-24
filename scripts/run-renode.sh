4515ed240cf55bb2f44487480c06ae90f464f805
marang <1550038+marang@users.noreply.github.com>
2025-08-24 23:09:02 +0200

    refactor renode platform to define peripherals

--- scripts/run-renode.sh
+++ scripts/run-renode.sh
@@ -18,6 +18,5 @@ make build
   -e 'mach create "stm32l432-keyboard"' \
   -e 'machine LoadPlatformDescription @renode/stm32l432_keyboard.repl' \
   -e "sysbus LoadELF @$FIRMWARE" \
-  -e 'connector Connect sysbus.usb 1234' \
   -e 'start' \
   2>&1 | tee "$RENODE_LOG"
